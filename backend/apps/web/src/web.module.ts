1b0f688bf251453ba33d3de8dcf66592ea3e42b0
Jacob Goh <jacob.goh@123rf.com>
2021-03-21 11:57:25 +0800

    TWT-1: setup Postgres container. setup backend DB integration

--- backend/apps/web/src/web.module.ts
+++ backend/apps/web/src/web.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENV } from '../../../libs/env/env';
 import { WebController } from './web.controller';
 import { WebService } from './web.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: ENV.DB_HOST,
+      port: +ENV.DB_PORT,
+      username: ENV.DB_USERNAME,
+      password: ENV.DB_PASSWORD,
+      database: ENV.DB_NAME,
+      entities: [],
+      synchronize: true,
+      logging: ENV.isDev,
+    }),
+  ],
   controllers: [WebController],
   providers: [WebService],
 })
